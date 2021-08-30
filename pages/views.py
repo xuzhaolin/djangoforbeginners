dd940d57404b6b7426cc22fd07a1ef84b79d103f
照临 徐 <xuzhaolin@xuzhaolin.me>
2021-08-30 11:29:53 +0800

    add pages

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello World!')
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
